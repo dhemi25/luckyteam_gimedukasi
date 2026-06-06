deb1a2e9a34f27153e466f80f37f1a23f4465f38
Demi Iskandar <infobisnisdemi@gmail.com>
2026-06-06 14:56:40 +0700

    Asset Pas Bunga

--- Assets/Asset/Dede MI/Scripts/MainMenuController.cs
+++ Assets/Asset/Dede MI/Scripts/MainMenuController.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SceneManager.LoadScene("GameScene"); // nama scene game
     }
 
     // Update is called once per frame
@@ -13,4 +13,10 @@ public class MainMenuController : MonoBehaviour
     {
         
     }
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Keluar");
+    }
+
 }
